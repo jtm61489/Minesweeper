d53fca68945fdad58640e371f0173db6eaf47814
srussell123 <scottbrussell@gmail.com>
2013-08-08 21:34:56 +0900

    Early work on populating 9x9 panel

---
+++ Content/MinesweeperForm/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MinesweeperForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMines = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlMines
+            // 
+            this.pnlMines.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMines.Location = new System.Drawing.Point(58, 87);
+            this.pnlMines.Name = "pnlMines";
+            this.pnlMines.Size = new System.Drawing.Size(146, 146);
+            this.pnlMines.TabIndex = 14;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(90, 32);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 15;
+            this.btnStart.Text = "Start!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 285);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlMines);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMines;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Content/MinesweeperForm/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MinesweeperForm
+{
+    public class Square
+    {
+        private Button sqr;
+        private Game game;
+
+        public Square(Game game, int xPos, int yPos)
+        {
+            
+            sqr = new Button();
+            sqr.BackgroundImage = MinesweeperForm.Properties.Resources.ButtonUp;
+            sqr.Width = 16;
+            sqr.Height = 16;
+            sqr.FlatStyle = FlatStyle.Flat;
+
+            sqr.Left = xPos * sqr.Width;
+            sqr.Top = yPos * sqr.Height;
+
+            game.panel.Controls.Add(sqr);
+        }
+        
+    }
+}
